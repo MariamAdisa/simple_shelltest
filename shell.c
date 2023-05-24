4ef1b7fa675867541430154ee06881365ba34951
deeza19 <zainabadisa425@gmail.com>
2023-05-24 09:03:17 -0700

    shell b

---
+++ shell.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * my_shell - function that runs the shell
+ * Return: Always 0
+ */
+
+void my_shell(void)
+{
+	char *temp = NULL;
+	size_t bytes = 0;
+	ssize_t n_read;
+
+	while (1)
+	{
+		write(1, "Shell$", 7);
+		n_read = getline(&temp, &bytes, stdin);
+
+		if (n_read == -1)
+			return;
+
+		if (strcmp(temp, "exit\n") == 0)
+		{
+			exit(0);
+		}
+
+			int ntokens = 0;
+			char **argv = parse_token(temp, &ntokens);
+
+			printf("%d\n", ntokens);
+
+			executeprogram(argv);
+
+			free(argv);
+	}
+
+	free(temp);
+}
