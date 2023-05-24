6ebb313bd47159833f59e9a1894e0deb9eaa028d
deeza19 <zainabadisa425@gmail.com>
2023-05-24 08:24:54 -0700

    shell b

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - runs the shell program
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	my_shell();
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGS 64
+
+void my_shell();
+char **parse_token(char *temp, int *ntokens);
+char _strtok(char *str, const char *delim);
+void executeprogram(char **argv);
+char *_getenv(const char *name);
+
+#endif
